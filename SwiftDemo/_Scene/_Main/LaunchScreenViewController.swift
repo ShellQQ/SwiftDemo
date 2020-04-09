3b6fedaeeb3cf2738c76e4cc68b884228a7c9a64
ShellQQ <tomoe.chu@gmail.com>
2020-04-09 16:26:53 +0800

    Initial Commit

---
+++ SwiftDemo/_Scene/_Main/LaunchScreenViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LaunchScreenViewController.swift
+//  SwiftDemo
+//
+//  Created by Apple on 2020/4/9.
+//  Copyright © 2020 Nautilus. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+    
+    @IBOutlet weak var launchLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        labelAnimation()
+    }
+    
+    private func labelAnimation() {
+        UIView.animate(withDuration: 3,
+                       delay: 0,
+                       options: .curveEaseIn,
+                       animations: {
+                        self.launchLabel.transform = CGAffineTransform(rotationAngle: .pi)
+                        self.launchLabel.transform = .identity
+                        self.view.alpha = 0
+        }) { (finished) in
+            self.dismiss(animated: false)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
